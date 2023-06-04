443e048d43138be5d1d2225a75400110b6080dfb
jazc202 <jasminemconnolly@gmail.com>
2023-06-04 07:53:22 -0500

    changed code

--- jasmine-testing-exercises-starter/calculator/calculator.js
+++ jasmine-testing-exercises-starter/calculator/calculator.js
@@ -21,20 +21,59 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let loanAmount = document.getElementById('loan-amount');
+  let loanYears = document.getElementById('loan-years');
+  let loanRate = document.getElementById('loan-rate')
+
+  loanAmount.value = 0
+  loanYears.value = 0
+  loanRate.value = 0
+
+  let form = document.getElementById("calc-form");
+
+  
+  form.addEventListener('change', function() {
+    let vals = {
+      amount: loanAmount.value, 
+      years: loanYears.value, 
+      rate: loanRate.value
+    }
+  
+    if (loanAmount.value > 0 && loanYears.value > 0 && loanRate.value > 0) {
+      let monthly = calculateMonthlyPayment(vals)
+      updateMonthly(monthly)
+    }
+    
+  })
+  
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let princ = values.amount
+  let interest = values.rate /100
+  let payments = values.years * 12
+  
+  let dividend = (princ * interest)
+  let divisor = 1-(1+interest) ** -payments
+
+  let monthlyPayment = String(dividend/divisor)
+  let endPosition = monthlyPayment.indexOf('.') + 3
+  let output = `$${monthlyPayment.slice(0, endPosition)}`
+  return output;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  let monthlyUI = document.getElementById('monthly-payment')
+  monthlyUI.innerText = monthly
 }
