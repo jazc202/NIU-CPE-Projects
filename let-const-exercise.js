4bdc90d3e847a2ca8f4a92fbb60841ed40e025ad
jazc202 <jasminemconnolly@gmail.com>
2023-07-13 10:07:25 -0500

    Let-const exercise

---
+++ let-const-exercise.js
@@ -0,0 +1,19 @@
+/*ES5*/
+
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+/* ES2015 */
+const PI = 3.14;
+PI = 42 //not allowed
+
+
+/* QUIZ */
+// - What is the difference between ***var*** and ***let***?
+    // - var can be redeclared, let cannot
+// - What is the difference between **var** and ***const***?
+    // - var can be reassigned and redeclared, const cannot
+// - What is the difference between **let** and **const**?
+    // - let can be reassigned, const cannot
+// - What is hoisting?
+    // - when variables are used before they're declared
